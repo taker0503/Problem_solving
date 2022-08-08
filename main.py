bf919103d8317a1ebadd55e80d3dd68330fcb9d8
taker <dlxorud120@naver.com>
2022-08-08 23:19:32 +0900

    DFS&BFS.02

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
-from collections import deque
-N, M, K, X = map(int,input().split())
-node = [[]for _ in range(N+1)]
-count = [ -1 for _ in range(N+1)]
-result = []
-for i in range(M):
-    A, B = map(int,input().split())
-    node[A].append(B)
+N, M = map(int,input().split())
+graph = []
+ex = [[0] * M for _ in range(N)]
+dx = [-1, 0 , 1, 0]
+dy = [ 0 , 1, 0 , -1]
+result = 0
+for _ in range(M):
+    graph.append(list(map(int,input().split())))
 
-def solution():
-    queue = deque([X])
-    count[X] = 0
-    while queue:
-        x = queue.popleft()
-        for i in node[x]:
-            if count[i] == -1:
-                count[i] = count[x] + 1
-                if i not in queue:
-                    queue.append(i)
-    for i in range(N+1):
-        if count[i] == K:
-            result.append(i)
+def solution(cnt):
+    global result
+    if cnt == 3:
+        for i in range(M):
+            for j in range(N):
+                ex[i][j] = graph[i][j]
+        for i in range(M):
+            for j in range(N):
+                if ex[i][j] == 2:
+                    virus(i,j)
+        result = max(result, get_score())
+        return
 
-    return result if len(result) else -1
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] == 0:
+                graph[i][j] = 1
+                cnt += 1
+                solution(cnt)
+                graph[i][j] = 0
+                cnt -= 1
+    return result
 
 
+def get_score():
+    count = 0
+    for i in range(M):
+        for j in range(N):
+            if ex[i][j] == 0:
+                count += 1
+    return count
 
-print(solution())
+def virus(i,j):
+    for idx in range(4):
+        nx = i + dx[idx]
+        ny = j + dy[idx]
+        if nx < 0 or nx >= N or ny < 0 or ny >= M:
+            continue
+        if ex[nx][ny] == 0:
+            ex[nx][ny] = 2
+            virus(nx,ny)
+
+print(solution(0))
